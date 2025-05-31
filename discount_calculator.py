153c90e331762d65053798cb6c0269d47a87693a
MiguelBvr <miguelbrv.ul@gmail.com>
2025-05-31 13:11:36 -0300

    Update discount_calculator.py

--- discount_calculator.py
+++ discount_calculator.py
@@ -8,8 +8,8 @@ except ValueError:
 
 else:
 
-    verif_student = input('você é estudante? ').lower()
-    verif_cumpom = input('você tem desconto? ').lower()
+    verif_student = input('você é estudante? (sim/não) ').lower()
+    verif_cumpom = input('você tem desconto? (sim/não) ').lower()
 
 if idade <= 12:
     preco = 10.00
@@ -20,21 +20,27 @@ elif idade <= 17:
 elif idade <= 59:
     preco = 30.00
 
-else:
+elif idade <= 110:
     preco = 12.00
+
+else:
+    print('Essa idade não existe.')
 
 preco_com_desconto = preco
 
-if verif_student in ['sim', 'Sim', "SIM"]:
+
+if verif_student in ['sim', 's']:
     preco_com_desconto = preco * (1 - 0.50)
+    print('linha 34', preco_com_desconto)
 
-    if verif_cumpom in ['sim', 'Sim', "SIM"]:
-        preco_com_desconto = preco * (1 - 0.20)
-
-    if verif_student and verif_cumpom in ['sim', 'Sim', "SIM"]:
-        preco_com_desconto = preco * (1 - 0.70)
+if verif_cumpom in ['sim', 's']:
+    preco_com_desconto = preco_com_desconto * (1 - 0.20)
+    print('linha 38', preco_com_desconto)
 
 preco_final = round(preco_com_desconto, 2)
+
+
+print(f'valor final do ingresso é de R${preco_final}')
 
 
 print(f'valor final do ingresso é de R${preco_final}')
