98f0e1ba18589dea2b84fefce41c264f3862cbe5
MiguelBvr <miguelbrv.ul@gmail.com>
2025-06-01 18:58:28 -0300

    Update discount_calculator.py

--- discount_calculator.py
+++ discount_calculator.py
@@ -6,41 +6,42 @@ try:
 except ValueError:
     print('Escreva sua idade apenas com numeros.')
 
-else:
 
-    if idade <= 12:
+
+if idade <= 12:
         preco = 10.00
 
-    elif idade <= 17:
+elif idade <= 17:
         preco = 15.00
 
-    elif idade <= 59:
+elif idade <= 59:
         preco = 30.00
 
-    elif idade <= 110:
+elif idade <= 110:
         preco = 12.00
 
-    else:
+else:
         print('Essa idade não existe.')
 
-    verif_student = input('você é estudante? (sim/não) ').lower()
-    if verif_student not in ['sim', 's', 'não', 'n']:
+verif_student = input('você é estudante? (sim/não) ').lower()
+if verif_student not in ['sim', 's', 'não', 'n']:
         print('Apenas sim e não')
+else:
+
+    verif_cumpom = input('você tem desconto? (sim/não) ').lower()
+    if verif_cumpom not in ['sim', 's', 'não', 'n']:
+            print('Apenas sim e não')
     else:
 
-        verif_cumpom = input('você tem desconto? (sim/não) ').lower()
-        if verif_cumpom not in ['sim', 's', 'não', 'n']:
-            print('Apenas sim e não')
-        else:
+        preco_com_desconto = preco
 
-            preco_com_desconto = preco
-
-            if verif_student in ['sim', 's']:
+        if verif_student in ['sim', 's']:
                 preco_com_desconto = preco * (1 - 0.50)
 
-            if verif_cumpom in ['sim', 's']:
+        if verif_cumpom in ['sim', 's']:
                 preco_com_desconto = preco_com_desconto * (1 - 0.20)
 
-            preco_final = round(preco_com_desconto, 2)
+        preco_final = round(preco_com_desconto, 2)
 
-            print(f'valor final do ingresso é de R${preco_final}')
+        print(f'valor final do ingresso é de R${preco_final}')
+
